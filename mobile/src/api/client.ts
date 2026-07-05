ec5016237dd575a37696a2f3b0c5e107dca8bbb6
Georgio Chahoud <georgioji@gmail.com>
2026-07-05 15:14:54 -0600

    [mobile] Update API URL to https://api.datebrownies.com

--- mobile/src/api/client.ts
+++ mobile/src/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
 const API_URL =
-  'http://10.0.2.2:8000/api/v1'; // Android emulator -> host
+  'https://api.datebrownies.com/api/v1';
 
 const apiClient = axios.create({
   baseURL: API_URL,
